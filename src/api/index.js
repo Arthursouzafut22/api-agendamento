fa0db62a41fe5f99171b17bcb67ed08e575aa080
Arthursouzafut22 <arthursouzafut22@gmail.com>
2025-08-08 19:49:59 -0300

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { pool } from "../models/db.js";
+
+const app = express();
+
+app.get("/api/services", async (_, res) => {
+  try {
+    const query = await pool.query("SELECT * FROM servicos");
+    res.status(200).json(query.rows);
+  } catch (erro) {
+    console.error("Erro ao buscar serviços:", erro);
+    res.status(500).json({ error: "Erro em buscar servicos" });
+  }
+});
+
+
+app.all("*", (_, res) => {
+  res.status(404).json({ error: "Rota não encontrada" });
+});
+
+export default app;
