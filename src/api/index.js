4901245b8310c8564d1bc3c1e06e2a57a059b66b
Arthursouzafut22 <arthursouzafut22@gmail.com>
2025-09-03 10:21:23 -0300

    Novas alteração

--- src/api/index.js
+++ src/api/index.js
@@ -6,11 +6,12 @@ import path from "path";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3005;
 const app = express();
+const PORT = process.env.PORT || 3005;
 app.use(express.json());
 app.use(cors());
 app.use(router);
 app.use("/imagem", express.static(path.resolve("img/image")));
+
 
 app.listen(PORT, () => console.log("Api rodando..."));
