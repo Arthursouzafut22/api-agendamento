87814f56751bf459e91b214746024a400cdebaad
Arthursouzafut22 <arthursouzafut22@gmail.com>
2025-09-03 10:51:19 -0300

    Modificando arquivo js

--- src/models/BookingModel.js
+++ src/models/BookingModel.js
@@ -1,7 +1,14 @@
 import { pool } from "./db.js";
 
 class Booking {
-  createBooking = async ({ Nome, Telefone, Servico, Data, Horario, usuario_id }) => {
+  createBooking = async ({
+    Nome,
+    Telefone,
+    Servico,
+    Data,
+    Horario,
+    usuario_id,
+  }) => {
     const query = await pool.query(
       "INSERT INTO agendamentos (cliente, telefone, servico, data, horario, usuario_id) VALUES ($1,$2,$3,$4,$5,$6)",
       [Nome, Telefone, Servico, Data, Horario, usuario_id]
